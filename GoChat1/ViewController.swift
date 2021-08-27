619345a180f732966493ff7498077323a5661d92
long nguyen <elongmusk39@gmail.com>
2021-08-27 09:34:37 -0700

    Initial Commit

---
+++ GoChat1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoChat1
+//
+//  Created by Long Nguyen on 8/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
